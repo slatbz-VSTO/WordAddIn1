a549ddd4d47a53bd9368bb7ad5ed35614ec2e216
bslattz <robert_slattery@hotmail.com>
2017-03-19 12:19:12 +1100

    Add project files.

---
+++ Utility/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Mime;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Utility
+{
+    public static class MainTimer
+    {
+        public static void onElapsed (Word.Application state, System.Timers.ElapsedEventArgs e)
+        {
+            if (state.Documents.Count > 0)
+            {
+                Debug.WriteLine("Woof!");
+                var doc = state.ActiveDocument;
+                Word.InlineShapes shps;
+                Word.Paragraphs pars;
+                try
+                {
+                    pars = doc.Paragraphs;
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+                var pars2 = pars.Cast<Word.Paragraph>()
+                    .Where(p => p.OutlineLevel == Word.WdOutlineLevel.wdOutlineLevelBodyText)
+                    .Select(p => p) // do stuff with the selected parragraphs...
+                    .ToList();
+            }
+        }
+    }
+}
---
+++ WordAddIn1/ThisAddIn.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Utility;
+
+namespace WordAddIn1
+{
+    public partial class ThisAddIn
+    {
+        private void MainTimer_Elapsed (object sender, System.Timers.ElapsedEventArgs e)
+        {
+            var scrnUpdating = Application.ScreenUpdating;
+            Application.ScreenUpdating = false;
+
+            if (Application.Documents.Count > 0)
+            {
+                var doc = Application.ActiveDocument;
+                Word.InlineShapes shps;
+                Word.Paragraphs pars;
+                try
+                {
+                    pars = doc.Paragraphs;
+                }
+                catch (Exception)
+                {
+                    if (scrnUpdating)
+                        Application.ScreenUpdating = true;
+                    return;
+                }
+                var pars2 = pars.Cast<Word.Paragraph>().ToList();
+                foreach (var obj in pars2)
+                {
+                    if (obj.OutlineLevel == Word.WdOutlineLevel.wdOutlineLevelBodyText)//PROBLEM HERE
+                    {
+
+                    };
+                }
+            }
+            if (scrnUpdating)
+                Application.ScreenUpdating = true;
+        }
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Timer timer = new Timer(2000);
+            timer.Elapsed += (s, t) =>
+            {
+                var scrnUpdating = Application.ScreenUpdating;
+                Application.ScreenUpdating = false;
+                MainTimer.onElapsed(Application, t);
+                if (scrnUpdating)
+                    Application.ScreenUpdating = true;
+            };
+            timer.Start();
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
